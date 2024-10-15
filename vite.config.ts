eef3571fd1d2291c9e02d965e641f0cdde116e80
Zongo Maqutu <zmaqutu@gmail.com>
2024-10-15 17:20:59 +0200

    installed tailwind dep and updated config files

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/scroll-splash-smart-tv/",
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  }
 })
